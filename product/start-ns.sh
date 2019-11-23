d6a91eb93cbf2cea411f9f7e13029f00bcc24e9f
BaoJingyu <baojingyu@live.cn>
2019-11-23 11:16:59 +0800

    v1.0

---
+++ product/start-ns.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+## Main
+if [ $# -lt 3 ]; then
+    echo "Usage: sh $0 DATA_HOME ROCKETMQ_VERSION BASE_IMAGE"
+    exit -1
+fi
+
+DATA_HOME=$1
+ROCKETMQ_VERSION=$2
+BASE_IMAGE=$3
+
+## Show Env Setting
+echo "ENV Setting: "
+echo "DATA_HOME=${DATA_HOME} ROCKETMQ_VERSION=${ROCKETMQ_VERSION}"
+
+# Start nameserver
+start_namesrv()
+{
+    TAG_SUFFIX=$1
+    docker run -d -v ${DATA_HOME}/logs:/home/rocketmq/logs \
+      --name rmqnamesrv \
+      -p 9876:9876 \
+      rocketmqinc/rocketmq:${ROCKETMQ_VERSION}${TAG_SUFFIX} \
+      sh mqnamesrv
+}
+
+case "${BASE_IMAGE}" in
+    alpine)
+        start_namesrv -alpine
+    ;;
+    centos)
+        start_namesrv
+    ;;
+    *)
+        echo "${BASE_IMAGE} is not supported, supported base images: centos, alpine"
+        exit -1
+    ;;
+esac
---
+++ templates/play-docker-compose.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+RMQ_CONTAINER=$(docker ps -a|awk '/rmq/ {print $1}')
+if [[ -n "$RMQ_CONTAINER" ]]; then
+   echo "Removing RocketMQ Container..."
+   docker rm -fv $RMQ_CONTAINER
+   # Wait till the existing containers are removed
+   sleep 5
+fi
+
+prepare_dir()
+{
+    dirs=("docker-compose/data/namesrv/logs" "docker-compose/data1/broker/logs" "docker-compose/data1/broker/store" "docker-compose/data2/broker/logs" "docker-compose/data2/broker/store" "docker-compose/data3/broker/logs" "docker-compose/data3/broker/store" "docker-compose/data4/broker/logs" "docker-compose/data4/broker/store")
+
+    for dir in ${dirs[@]}
+    do
+        if [ ! -d "`pwd`/${dir}" ]; then
+            mkdir -p "`pwd`/${dir}"
+            chmod a+rw "`pwd`/${dir}"
+        fi
+    done
+}
+
+prepare_dir
+
+# Run nameserver and broker
+docker-compose -f ./docker-compose/docker-compose.yml up -d
